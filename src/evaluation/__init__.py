418a8f3aaeeee8a625666f8d941508f1ed9ec760
Faran Butt <faranbutt789@gmail.com>
2026-05-15 15:05:07 +0500

    added

--- src/evaluation/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.utils.hf_utils import login_to_hf
-from src.utils.config_loader import load_config
-
-config_eval = load_config('configs/evaluate.yaml')
-config_data = load_config('configs/data.yaml')
--- src/training/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.utils.hf_utils import login_to_hf
-from src.utils.config_loader import load_config
-
-config_train = load_config('configs/train.yaml')
-config_data = load_config('configs/data.yaml')
